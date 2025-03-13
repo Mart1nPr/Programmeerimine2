c6ece24dc6e44a1012d3c24f3d13029d908f3784
Martin Pruus <martin.pruus@tptlive.ee>
2025-03-13 15:28:08 +0200

    Add Web API (not working)

---
+++ KooliProjekt/Controllers/UsersApiController.cs
@@ -0,0 +1,84 @@
+﻿using KooliProjekt.Data;
+using KooliProjekt.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KooliProjekt.Controllers
+{
+    [Route("api/Users")]
+    [ApiController]
+    public class UsersApiController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersApiController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {
+            var result = await _userService.List(1, 10000);
+            return Ok(result.Results);
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> Get(int id)
+        {
+            var user = await _userService.Get(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public async Task<ActionResult<User>> Post([FromBody] User user)
+        {
+            await _userService.Save(user);
+
+            return CreatedAtAction(nameof(Get), new { id = user.Id }, user);
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingUser = await _userService.Get(id);
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+
+            await _userService.Save(user);
+
+            return NoContent();
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await _userService.Get(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            await _userService.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
